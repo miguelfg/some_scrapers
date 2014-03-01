1d0617012572e106a2a0378f2f9f626009d08d43
miguelfg <mfiandor@gmail.com>
2014-03-01 10:57:58 +0100

    using requests, fails second step

--- concejales.py
+++ concejales.py
@@ -23,9 +23,9 @@ def using_urllib_1(province=1):
         for opt in options[:3]:
             if '--' in opt.get_text():
                 continue
-            print opt.get('value'), opt.get_text()
+            #print opt.get('value'), opt.get_text()
             ine_code = opt.get('value')
-            response2 = using_urllib_2(province, ine_code, cookie=cookie)
+            using_urllib_2(province, ine_code, cookie=cookie)
 
     except urllib2.URLError, e:
         if not hasattr(e, "code"):
@@ -41,6 +41,7 @@ def using_urllib_2(province=2, ine_code=17978, cookie=''):
     try:
         req = urllib2.Request("https://ssweb.seap.minhap.es/portalEELL/consulta_alcaldes")
         req.add_header("X-Requested-With", "XMLHttpRequest")
+        #req.add_header("Cookie", cookie)
         req.add_header("Cookie", "portalEELL=uos1gss8sr9e3scgo16m9pt1o7; ssoEELL=25fma5i9cabtduqcvaam8bs2q7; _ga=GA1.3.1489095445.1392115052; BALANCEID=balancer.prophpmodmz01")
         body = r"consulta_alcalde%5B_csrf_token%5D=fc69f17c058187dc3b02a79c5a02d6bb&consulta_alcalde%5Bid_provincia%5D="+str(province)+"&consulta_alcalde%5Bid_entidad%5D="+str(ine_code)
 
@@ -73,25 +74,38 @@ def using_requests_1(province):
         for opt in options[:3]:
             if '--' in opt.get_text():
                 continue
-            print opt.get('value'), opt.get_text()
+            #print opt.get('value'), opt.get_text()
             ine_code = opt.get('value')
-            response2 = using_requests_2(province, ine_code, cookie)
+            using_requests_2(province, ine_code, cookie)
 
 def using_requests_2(province, ine_code, cookie):
     url = 'https://ssweb.seap.minhap.es/portalEELL/consulta_alcaldes'
     headers = {'X-Requested-With': 'XMLHttpRequest',
                "Referer": "https://ssweb.seap.minhap.es/portalEELL/consulta_alcaldes",
-               'Cookie': cookie
+               #'Cookie': cookie
     }
-    #body = r"consulta_alcalde%5B_csrf_token%5D=fc69f17c058187dc3b02a79c5a02d6bb&consulta_alcalde%5Bid_provincia%5D=2&consulta_alcalde%5Bid_entidad%5D=17978"
+    #cookies = dict(Cookie = r"portalEELL=uos1gss8sr9e3scgo16m9pt1o7; ssoEELL=25fma5i9cabtduqcvaam8bs2q7; _ga=GA1.3.1489095445.1392115052; BALANCEID=balancer.prophpmodmz01")
+    cookies = dict(
+                    BALANCEID=r"balancer.prophpmodmz01",
+                    path=r"/",
+                    domain=r"ssweb.seap.minhap.es",
+                    #portalEELL=r"uos1gss8sr9e3scgo16m9pt1o7",
+                    #ssoEELL=r"25fma5i9cabtduqcvaam8bs2q7",
+                    #_ga=r"GA1.3.1489095445.1392115052",
+    )
     body = r"consulta_alcalde%5B_csrf_token%5D=fc69f17c058187dc3b02a79c5a02d6bb&consulta_alcalde%5Bid_provincia%5D="+str(province)+"&consulta_alcalde%5Bid_entidad%5D="+str(ine_code)
-    body = "consulta_alcalde%5B_csrf_token%5D=fc69f17c058187dc3b02a79c5a02d6bb&consulta_alcalde%5Bid_provincia%5D="+str(province)+"&consulta_alcalde%5Bid_entidad%5D="+str(ine_code)
+    #body = r"consulta_alcalde%5B_csrf_token%5D=fc69f17c058187dc3b02a79c5a02d6bb&consulta_alcalde%5Bid_provincia%5D=2&consulta_alcalde%5Bid_entidad%5D=17978"
     #payload = {'body': body}
 
-    response = requests.post(url, headers=headers, data=body, verify=False)
+    response = requests.post(url,
+                             headers=headers,
+                             cookies=cookies,
+                             data=body,
+                             verify=False)
     print response.status_code
     print response.headers['set-cookie']
     print response.text
+    #print response.headers
 
 #lib REQUESTS
 response = using_requests_1(3)
